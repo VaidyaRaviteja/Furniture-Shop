a743a6aec5352bb93fa245573f4fe38e139771c3
VaidyaRaviteja <131844294+VaidyaRaviteja@users.noreply.github.com>
2025-05-16 15:21:55 +0530

    Added Configuration

---
+++ Config.js
@@ -0,0 +1,46 @@
+    // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.7.3/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAf5oBZW_LB2kXA_CO3xieX7P2MHzwPCdY",
+    authDomain: "e-commerce-78f8e.firebaseapp.com",
+    projectId: "e-commerce-78f8e",
+    storageBucket: "e-commerce-78f8e.firebasestorage.app",
+    messagingSenderId: "626083489045",
+    appId: "1:626083489045:web:00a96b05be0a5c77b1351c",
+    measurementId: "G-VV5XQRR69X"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
